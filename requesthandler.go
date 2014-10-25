1a857410220ca8d9739a577f31ac3d5cacc64ec3
Valere JEANTET <valere.jeantet@gmail.com>
2014-10-25 15:44:23 +0200

    comments on default handlers

--- requesthandler.go
+++ requesthandler.go
@@ -1,11 +1,17 @@
 package ldapserver
 
+// handleBindRequest is the default handler for BindRequests, It always
+// returns a Success
+// use Server.SetBindHandler() to implement a custom handler
 func handleBindRequest(w BindResponse, r *BindRequest) {
 	w.ResultCode = LDAPResultSuccess
 	w.Send()
 	return
 }
 
+// handleSearchRequest is the default handler for SearchRequest, It always
+// returns a Success with no entries
+// use Server.SetSearchHandler() to implement a custom handler
 func handleSearchRequest(w SearchResponse, r *SearchRequest) {
 	w.ResultCode = LDAPResultSuccess
 	w.Send()
