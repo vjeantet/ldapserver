1e0e643e3fa6ea7ab6f0f6a9e5baf53b226d0352
Valere JEANTET <valere.jeantet@gmail.com>
2015-08-21 01:46:47 +0200

    remove unused constant and types

--- messageldap.go
+++ messageldap.go
@@ -1,20 +1,11 @@
 package ldapserver
 
-type Controls []Control
-
-type Control struct {
-	controlType  string
-	criticality  bool
-	controlValue string
-}
-
 // OCTETSTRING ASN.1 types
 type OCTETSTRING string
 
 // LDAPOID is a notational convenience to indicate that the
 // permitted value of this string is a (UTF-8 encoded) dotted-decimal
 // representation of an OBJECT IDENTIFIER.  Although an LDAPOID is
-type LDAPOID string
 
 // LDAPDN is defined to be the representation of a Distinguished Name
 // (DN) after encoding according to the specification
@@ -56,42 +47,4 @@ type PartialAttributeList []PartialAttribute
 
 func (l *PartialAttributeList) add(p PartialAttribute) {
 	*l = append(*l, p)
-}
-
-//
-//        Attribute ::= PartialAttribute(WITH COMPONENTS {
-//             ...,
-//             vals (SIZE(1..MAX))})
-type Attribute PartialAttribute
-
-//
-//        AttributeList ::= SEQUENCE OF attribute Attribute
-type AttributeList []Attribute
-
-func (p *Attribute) GetDescription() AttributeDescription {
-	return p.type_
-}
-func (p *Attribute) GetValues() []AttributeValue {
-	return p.vals
-}
-
-//
-//        AssertionValue ::= OCTET STRING
-type AssertionValue OCTETSTRING
-
-//
-//        AttributeValueAssertion ::= SEQUENCE {
-//             attributeDesc   AttributeDescription,
-//             assertionValue  AssertionValue }
-type AttributeValueAssertion struct {
-	attributeDesc  AttributeDescription
-	assertionValue AssertionValue
-}
-
-func (a *AttributeValueAssertion) GetName() string {
-	return string(a.attributeDesc)
-}
-
-func (a *AttributeValueAssertion) GetValue() string {
-	return string(a.assertionValue)
 }
