50d796e48876adb36387634abf738560249fbda4
Jiajie Chen <c@jia.je>
2021-05-17 17:14:33 +0800

    Replace panic with error

--- packet.go
+++ packet.go
@@ -86,7 +86,8 @@ func readTagAndLength(conn *bufio.Reader, bytes *[]byte) (ret ldap.TagAndLength,
 	//	}
 	// We are expecting the LDAP sequence tag 0x30 as first byte
 	if b != 0x30 {
-		panic(fmt.Sprintf("Expecting 0x30 as first byte, but got %#x instead", b))
+		err = ldap.SyntaxError{fmt.Sprintf("expecting 0x30 as first byte, but got %#x instead", b)}
+		return
 	}
 
 	b, err = readBytes(conn, bytes, 1)
